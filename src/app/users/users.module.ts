c08067188b76a55faf63f995f0a8ee716591aef7
Shubham Gupta <shubham.gupta@mobicules.com>
2020-10-18 18:52:13 +0530

    add sign up comp

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [LoginComponent, SignupComponent],
   imports: [
     CommonModule
   ],
-  exports:[LoginComponent],
+  exports:[LoginComponent, SignupComponent],
 })
 export class UsersModule { }
